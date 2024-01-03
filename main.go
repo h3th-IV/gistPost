172cd9dbc8ea1c7252371ccdd8ed4c132e8ff54c
B2N10X30 <samuelbonux10@gmail.com>
2024-01-03 18:45:42 +0100

    house cleaning et stuff

--- main.go
+++ main.go
@@ -9,16 +9,21 @@ import (
 )
 
 func main() {
+	//init env varaibles
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("error loading files: %v", err)
 	}
 
+	//start database
 	dB, err := database.InitDB()
 	if err != nil {
 		log.Printf("Err connecting to database: %v", err)
 	}
+	//always remember to shut it down
 	defer database.CloseDB()
+
+	//get comments
 	comments := database.Comments()
 	for _, comment := range *comments {
 		_, err = dB.Exec(`insert into comments (postid, id , name, email, body) values ($1, $2, $3, $4, $5)`, comment.PostID, comment.ID, comment.Name, comment.Email, comment.Body)
@@ -32,6 +37,7 @@ func main() {
 		}
 	}
 
+	//get user1 post
 	userPost := database.Post()
 	_, err = dB.Exec(`INSERT INTO posts(userid, id, title, body) values ($1, $2, $3, $4);`, userPost.UserId, userPost.Id, userPost.Title, userPost.Body)
 	fmt.Println("postTItle: ", userPost.Title)
